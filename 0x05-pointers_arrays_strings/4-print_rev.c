50ef5de552cdf277de1a89b32af0a1f712241cb9
Melvin1580 <emma42daniel@gmai.com>
2022-12-20 05:07:44 -0800

    Adding the file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,12 @@ void print_rev(char *s)
 
 	while (count >= 0)
 	{
-		if (s[count] ==
-'\0')
+		if (s[count] == '\0')
 			break;
 		count++;
 	}
 
-	for (count--; count >= 0;
-count--)
+	for (count--; count >= 0; count--)
 		_putchar(s[count]);
 	_putchar('\n');
 }
