92cce03e1c7312802e7d063759f8196da7e6a6e1
Melvin1580 <emma42daniel@gmai.com>
2022-12-20 03:32:08 -0800

    Adding 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,16 +5,13 @@
  * @str: input string
  * Return: no return
  */
-
 void _puts(char *str)
 {
 	int count;
 
-	for (count = 0; str[count]
-!= '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 	{
-
-_putchar(str[count]);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
 }
